b51b366799926a6b70745dbdc8decca67a120d91
NishaVThorbole <97584289+NishaVThorbole@users.noreply.github.com>
2022-02-22 17:56:21 +0530

    Update UITest.java

--- jenkinsIntegration/src/test/java/mavenForJenkins/UITest.java
+++ jenkinsIntegration/src/test/java/mavenForJenkins/UITest.java
@@ -17,6 +17,7 @@ public class UITest {
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+		system.out.println("chnged for jenkins");
 		driver.quit();
 	}
 }
